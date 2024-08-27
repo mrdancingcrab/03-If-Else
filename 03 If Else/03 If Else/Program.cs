ee21bd7db664f66e469d019fc3e71f287dda3a06
mrdancingcrab <jaederberg@gmail.com>
2024-08-27 17:31:58 +0200

    Even or Odd

--- 03 If Else/03 If Else/Program.cs
+++ 03 If Else/03 If Else/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Ange en siffra:");
+int.TryParse(Console.ReadLine(), out int siffra);
+
+if (siffra % 2 == 0)
+{
+    Console.WriteLine("Din siffra är jämn");
+}
+else
+    Console.WriteLine("Din siffra är ojämn");
+
+
