3bcd0c108166d0bf6c8e805a395e412414efd890
mrdancingcrab <jaederberg@gmail.com>
2024-08-27 20:29:48 +0200

    Password Checker

--- 03 If Else/03 If Else/Program.cs
+++ 03 If Else/03 If Else/Program.cs
@@ -1,35 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Input a number between 1-7:");
-double.TryParse(Console.ReadLine(), out double num1);
+string password = "HEJ123";
+Console.WriteLine("Enter password:");
+string input = Console.ReadLine();
 
-if (num1 == 1)
+
+if (input == password)
 {
-    Console.WriteLine("Monday");
-}
-else if (num1 == 2)
-{
-    Console.WriteLine("Tuesday");
-}
-else if (num1 == 3)
-{
-    Console.WriteLine("Wednesday");
-}
-else if (num1 == 4)
-{
-    Console.WriteLine("Thursday");
-}
-else if (num1 == 5)
-{
-    Console.WriteLine("Friday");
-}
-else if (num1 == 6)
-{
-    Console.WriteLine("Saturday");
-}
-else if (num1 == 7)
-{
-    Console.WriteLine("Sunday");
+    Console.WriteLine("Access granted");
 }
 else
-    Console.WriteLine("ERROR");
+    Console.WriteLine("Access denied");
+
