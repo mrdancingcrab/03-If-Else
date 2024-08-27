ef883be11bf6ed81d1449bdbcff23e17e4b9afaa
mrdancingcrab <jaederberg@gmail.com>
2024-08-27 19:28:43 +0200

    Leap Year Checker

--- 03 If Else/03 If Else/Program.cs
+++ 03 If Else/03 If Else/Program.cs
@@ -1,24 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Fill in your score between 0-100:");
-int.TryParse(Console.ReadLine(), out int score);
 
-if (score >= 90)
+Console.WriteLine("Pls put in a year:");
+int.TryParse(Console.ReadLine(), out int year);
+
+if ((year % 4 == 0 && year % 400 == 0) || (year % 4 == 0 && !(year % 100 == 0)))
 {
-    Console.WriteLine("A");
-}
-else if (score < 90 && score >= 80)
-{
-    Console.WriteLine("B");
-}
-else if (score < 80 && score >= 70)
-{
-    Console.WriteLine("C");
-}
-else if (score < 70 && score >= 60)
-{
-    Console.WriteLine("D");
+        Console.WriteLine("This IS a leap year");
 }
 else
-    Console.WriteLine("F");
-   
+    Console.WriteLine("This is NOT a leap year");
 
+   
